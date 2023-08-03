bde7d3f643b12f57b47348c770612cd80a3e86de
Mariia Paraketsova <paraketsova@gmail.com>
2023-08-03 12:30:13 +0200

    Add questions list

--- question.js
+++ question.js
@@ -1,13 +1,3 @@
-// class Question { // TODO: DELETE
-//   constructor (data) {
-//     this.question = data.question;
-//     this.answers = data.answers;
-//     this.correct_answers = data.correct_answers;
-//   }
-// }
-
-
-// NEW VERSION!!!
 class Question {
   constructor (data) {
     this.question = data.question;
